3a90f7cc1ad718a9d9a51ee1492ff2102ecca085
Piotr Gagorowski <piotrgagorowski19@gmail.com>
2024-03-18 22:18:00 +0100

    First devise-v1

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+User.create!(email: 'patient2@example.com', password: 'password', role: :patient)
+User.create!(email: 'admin@example.com', password: 'password', role: :admin)
+User.create!(email: 'doctor@example.com', password: 'password', role: :doctor)
+User.create!(email: 'labworker@example.com', password: 'password', role: :labworker)
