2c42c3efc5c2adbf1b5438874c16f1db3a120631
Piotr Gagorowski <piotrgagorowski19@gmail.com>
2024-04-10 14:45:44 +0200

    Mailer works

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,21 +9,24 @@ Rails.application.configure do
   config.enable_reloading = true
 
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.perform_deliveries = true
-config.action_mailer.raise_delivery_errors = true
-config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 587,
-  domain:               'gmail.com',
-  user_name:            'rajbogdan94@gmail.com',
-  password:             'mrzs fhbt qglm ncvk',
-  authentication:       'plain',
-  enable_starttls_auto: true,
-}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default charset: "utf-8"
+  
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'rajbogdan94@gmail.com',
+    password:                   'mrzs fhbt qglm ncvk',
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    # openssl_verify_mode: 'none'
+  }
+  
 
 
 
