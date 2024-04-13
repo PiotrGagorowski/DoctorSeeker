3eec706084fa65c38c8c92bc9f3d99cf08911ba8
Viborcia <susuniak@gmail.com>
2024-04-13 12:34:09 +0200

    Session_controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :appointments
   resources :comments
   resources :medical_files
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   #root "medical_files#index"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -30,5 +30,16 @@ Rails.application.routes.draw do
 
   get 'lab', to: 'main#lab', as: 'lab'
 
+  # Doctor
   get 'doctor', to: 'users#doctor'
+  
+  #Patient
+  get 'patient', to: 'users#patient'
+
+  #Labworker
+  get 'labworker', to: 'users#labworker'
+
+  #Admin
+  get 'admin', to: 'users#admin'
+
 end
