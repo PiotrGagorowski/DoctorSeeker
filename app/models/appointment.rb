2039aa68bc3e8f7f7a29b75c35e3a25fcc5e7929
Patryk Wawrzacz <tatrenings@gmail.com>
2024-04-18 10:51:35 +0200

    added doctor functionalities

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,5 +1,9 @@
 class Appointment < ApplicationRecord
+    belongs_to :doctor, class_name: 'User', foreign_key: 'doctor_user_id'
     has_many :user_appointments
     has_many :patients, through: :user_appointments, source: :patient
-    has_many :doctors, through: :user_appointments, source: :doctor
-end
+
+    scope :reserved, -> { joins(:user_appointments).distinct }
+    scope :free, -> { where.not(id: joins(:user_appointments).select(:appointment_id)) }
+  
+  end
